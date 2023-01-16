7b484e5747f85d37c5df97b59193d03605c584e0
kalpee <shogo.galleria.game@gmail.com>
2023-01-17 00:29:50 +0900

    second

--- laratodo/routes/web.php
+++ laratodo/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,40 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get("/dashboard", function () {
+    return view("dashboard");
+})
+    ->middleware(["auth"])
+    ->name("dashboard");
+
+Route::get("/", [HomeController::class, "index"])->name("home");
+
+Route::get("/folders/{id}/tasks", [TaskController::class, "index"])->name(
+    "tasks.index"
+);
+
+Route::get("/folders/create", [
+    FolderController::class,
+    "showCreateForm",
+])->name("folders.create");
+
+Route::post("/folders/create", [FolderController::class, "create"]);
+
+Route::get("/folders/{id}/tasks/create", [
+    TaskController::class,
+    "showCreateForm",
+])->name("tasks.create");
+
+Route::post("/folders/{id}/tasks/create", [TaskController::class, "create"]);
+
+Route::get("/folders/{id}/tasks/{task_id}/edit", [
+    TaskController::class,
+    "showEditForm",
+])->name("tasks.edit");
+
+Route::post("/folders/{id}/tasks/{task_id}/edit", [
+    TaskController::class,
+    "edit",
+]);
+
+require __DIR__ . "/auth.php";

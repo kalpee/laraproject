337b6aa4a7fc0cb3ff23f85b4a0ff233ac2dbc72
kalpee <shogo.galleria.game@gmail.com>
2023-01-18 19:25:19 +0900

    login

--- laratodo/app/Http/Controllers/HomeController.php
+++ laratodo/app/Http/Controllers/HomeController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Folder;
+use Illuminate\Support\Facades\Auth;
+use function PHPUnit\Framework\isEmpty;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view("home");
+        $id = Auth::id();
+        $current_folder = Folder::where("user_id", $id)->get();
+
+        if ($current_folder->empty()) {
+            return redirect()->route("tasks.index", [
+                "id" => $id,
+            ]);
+        } else {
+            return view("home");
+        }
     }
 }
